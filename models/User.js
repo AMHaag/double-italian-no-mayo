247206f3e33c8a798bbb77e24d35ef6d0ec37a8d
Aaron Haag <101981364+AMHaag@users.noreply.github.com>
2022-08-25 16:48:58 -0400

    basic user model attributes written, hooks still missing

---
+++ models/User.js
@@ -0,0 +1,38 @@
+const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+const sequelize = require('../config/connection');
+
+
+class User extends Model {
+  checkPassword(pw) {
+    return bcrypt.compareSync(pw, this.password);
+  }
+}
+
+
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate:{
+        len:[5]
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [8],
+      },
+    },
+  },
+);
+
+module.exports = User;
