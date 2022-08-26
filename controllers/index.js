1767d9f8684d61658fdd5fd36c51dabbca4b837b
Aaron Haag <101981364+AMHaag@users.noreply.github.com>
2022-08-26 16:40:04 -0400

    created homepage and dashboard routes

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,12 @@
 const router = require('express').Router();
 
 const apiRoutes = require('./api/');
+const homePageRoutes = require('./homepage-routes');
+const dashboardRoutes = require('./dashboard-routes');
 
 router.use('/api', apiRoutes);
+router.use('/', homePageRoutes);
+router.use('/dashboard', dashboardRoutes);
 
 router.use((req, res) => {
   res.status(404);
